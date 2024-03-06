a81f0bfd9ae98f7a4210b24b539a871b03b58f99
Mehebul02 <mehebul2123@gmail.com>
2024-03-06 22:18:26 +0600

    Es6 recap

---
+++ es6.js
@@ -0,0 +1,11 @@
+const numbers =[23,4,5,532509,3,22,33,3];
+const student={
+    name:'Mehebul Alif',
+    dept:'Computer Technology',
+    session:'20-21',
+    skill:['html css tailwind javascript and react']
+};
+const about =`My name is ${student.name} department of  ${student.dept} my roll is ${numbers[3]} session ${student.session} my skill ${student.skill}`;
+console.log(about)
+
+
--- funtamental.js
+++ funtamental.js
@@ -1,1 +1,15 @@
-
+//Condition
+// const somrat = 40;
+// if (somrat === 40) {
+//   console.log("paid the amount");
+// } else {
+//   console.log("No Paid");
+// }
+const classTime = {
+  firstPeriod: { sub: "innovation", time: "8AM" },
+  secondPeriod: ["System", "Network"],
+  lastPeriod: ["System", "Network"],
+};
+if (classTime.firstPeriod === firstPeriod) {
+  console.log("8am-9pm");
+}
